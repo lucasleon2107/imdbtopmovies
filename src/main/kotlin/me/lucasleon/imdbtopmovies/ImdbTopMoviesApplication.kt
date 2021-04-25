2f781b6aa3fc7a29fc01342bf9c81836f302d215
Lucas León <lucasleon2107@gmail.com>
2021-04-25 17:15:00 -0500

    Add Swagger Documentatio

--- src/main/kotlin/me/lucasleon/imdbtopmovies/ImdbTopMoviesApplication.kt
+++ src/main/kotlin/me/lucasleon/imdbtopmovies/ImdbTopMoviesApplication.kt
@@ -3,8 +3,10 @@ package me.lucasleon.imdbtopmovies
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @EnableCaching
+@EnableSwagger2
 @SpringBootApplication
 class ImdbTopMoviesApplication
 
